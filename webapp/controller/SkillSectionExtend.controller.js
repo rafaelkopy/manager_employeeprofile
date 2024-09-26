5746e3cfc37ff943683dfb9c6823b7bf3d0031d4
pascal-g38 <pascalgolomb@outlook.de>
2024-09-26 16:32:44 +0200

    SkillSection implemented, doesn't work properly yet

---
+++ webapp/controller/SkillSectionExtend.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    "sap/ui/core/mvc/ControllerExtension"
+    ], (ControllerExtension) => {
+	"use strict";
+    
+        return ControllerExtension.extend("manageremployeeprofile.controller.SkillSectionExtend", {
+        _sectionPrefix: "fe::CustomSubSection::SkillSection--",
+        override: {   
+            onInit() { 
+                 this.getView().byId(this._sectionPrefix + "gridListId")           
+              }
+            }      
+          });
+        });  
+    
