91beb97860f9f3d0d27c2bb7285b417e6f541909
pascal-g38 <pascalgolomb@outlook.de>
2024-10-16 16:09:06 +0200

    Only Skills of the chosen Employee are shown now

--- webapp/controller/SkillSectionExtend.controller.js
+++ webapp/controller/SkillSectionExtend.controller.js
@@ -1,15 +1,25 @@
 sap.ui.define([
-    "sap/ui/core/mvc/ControllerExtension"
-    ], (ControllerExtension) => {
+    "sap/ui/core/mvc/ControllerExtension",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+    ], (ControllerExtension, Filter, FilterOperator) => {
 	"use strict";
     
         return ControllerExtension.extend("manageremployeeprofile.controller.SkillSectionExtend", {
         _sectionPrefix: "fe::CustomSubSection::SkillSection--",
         override: {   
             onInit() { 
-                 this.getView().byId(this._sectionPrefix + "gridListId")    
-              }
-            }      
+                this.getView().byId(this._sectionPrefix + "gridListId");
+            },
+            onAfterRendering() {
+               const SkillsView = this.getView().byId(this._sectionPrefix + "gridListId");
+
+//               const Routing = ItemsBinding.getPr
+
+               const ItemsBinding = SkillsView.getBinding("items");
+               ItemsBinding.filter(new Filter({ path: 'EmployeeId', operator: FilterOperator.EQ , value1: '7dc9de58-3904-1edf-8ab2-04e6f6c9d632' }));
+            }
+           }      
           });
         });  
     
