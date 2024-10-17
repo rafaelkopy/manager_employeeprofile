2d3cb0a65f2df6c6e64239e515c08c4208318765
Rafael Kopytto <r.kopytto@mibs-ag.de>
2024-10-17 11:27:19 +0200

    override for routing

--- webapp/controller/SkillSectionExtend.controller.js
+++ webapp/controller/SkillSectionExtend.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/ControllerExtension",
     "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator"
-    ], (ControllerExtension, Filter, FilterOperator) => {
+    "sap/ui/model/FilterOperator",
+    "sap/ui/model/json/JSONModel"
+    ], (ControllerExtension, Filter, FilterOperator, JSONModel) => {
 	"use strict";
     
         return ControllerExtension.extend("manageremployeeprofile.controller.SkillSectionExtend", {
@@ -10,6 +11,12 @@ sap.ui.define([
         override: {   
             onInit() { 
                 this.getView().byId(this._sectionPrefix + "gridListId");
+                this.getView().setModel(
+                    new JSONModel({
+                        id: null,
+                    }),
+                    "customView"
+                )
             },
             onAfterRendering() {
                const SkillsView = this.getView().byId(this._sectionPrefix + "gridListId");
@@ -17,8 +24,15 @@ sap.ui.define([
 //               const Routing = ItemsBinding.getPr
 
                const ItemsBinding = SkillsView.getBinding("items");
+               const id = this.getView().getModel("customView").getProperty("/id")
                ItemsBinding.filter(new Filter({ path: 'EmployeeId', operator: FilterOperator.EQ , value1: '7dc9de58-3904-1edf-8ab2-04e6f6c9d632' }));
-            }
+            },
+              routing: {
+                async onAfterBinding() {
+                    this.getView().getModel("customView").setProperty("/id", (await this.getView().getBindingContext().requestProperty("EmployeeId")) )
+
+                }
+              }
            }      
           });
         });  
